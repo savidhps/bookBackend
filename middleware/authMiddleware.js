367316405f0db6cd93e5a5314f8fefece886be5f
savidhps <savidhps00@gmail.com>
2025-08-14 23:52:17 +0530

    auth edits

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 const protect = async (req, res, next) => {
-  let toke;
+  let token;
   if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
     try {
       token = req.headers.authorization.split(" ")[1];
