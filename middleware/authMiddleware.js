bef83c4425775aeec6c98fbc2dd404d0b304a1ab
savidhps <savidhps00@gmail.com>
2025-08-14 23:51:55 +0530

    book controler edits

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 const protect = async (req, res, next) => {
-  let token;
+  let toke;
   if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
     try {
       token = req.headers.authorization.split(" ")[1];
